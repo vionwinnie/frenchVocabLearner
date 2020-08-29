db402efd681aad1df1bd91cc9dd68e5c2bad78ce
mit <mit945.mc@gmail.com>
2020-08-28 23:20:26 -0700

    tryna fix

--- frenchVocabLearner/frenchVocabLearner/settings.py
+++ frenchVocabLearner/frenchVocabLearner/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hk++#))iii1w+hyxz-r!6dct=-p%b6%^y1dp(mrybxgg-r!55y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'frenchVocabLearner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config('DB_Engine'),
+        'NAME': config('DB_Name')
     }
 }
 
