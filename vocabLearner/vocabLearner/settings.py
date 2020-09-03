d38243ec93385d29b23dd6c8deefe32d333cf84c
mit <mit945.mc@gmail.com>
2020-09-03 12:42:31 -0700

    add a should be path under SECRET and FIREBASE_ORM_CERTIFICATE in comment

--- vocabLearner/vocabLearner/settings.py
+++ vocabLearner/vocabLearner/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = yaml.safe_load(open("/home/winnie/petProjects/credentials/config.yaml"))
+# SECRET_KEY = yaml.safe_load(open(os.path.join(BASE_DIR, "credentials/config.yaml")))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,6 +76,7 @@ WSGI_APPLICATION = 'vocabLearner.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 FIREBASE_ORM_CERTIFICATE = '/Users/mitchellchang/Documents/credentials/frenchvocablearner-6c6727c36e1c.json'
+# FIREBASE_ORM_CERTIFICATE = open(os.path.join(BASE_DIR ,'credentials/frenchvocablearner-6c6727c36e1c.json'))
 FIREBASE_ORM_BUCKET_NAME = 'frenchvocablearner.appspot.com'
 DATABASES = {
     'default': {
