a29836cf0fae6c76bc117b31fe9db8a594786ac0
mit <mit945.mc@gmail.com>
2020-09-03 12:08:17 -0700

    use relative paths to pass creds:

--- vocabLearner/vocabLearner/settings.py
+++ vocabLearner/vocabLearner/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = yaml.safe_load(open("/home/winnie/petProjects/credentials/config.yaml"))
-
+SECRET_KEY = yaml.safe_load(open(os.path.join(BASE_DIR, 'credentials/config.yml')))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -74,7 +73,7 @@ WSGI_APPLICATION = 'vocabLearner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-FIREBASE_ORM_CERTIFICATE = '/Users/mitchellchang/Documents/credentials/frenchvocablearner-6c6727c36e1c.json'
+FIREBASE_ORM_CERTIFICATE = open(os.path.join(BASE_DIR, 'credentials/frenchvocablearner-6c6727c36e1c.json'))
 FIREBASE_ORM_BUCKET_NAME = 'frenchvocablearner.appspot.com'
 DATABASES = {
     'default': {
